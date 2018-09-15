6680e14ce9048c6a845c786be9b5e079ae4eb173
Cedric Guillemet <cedric.guillemet@gmail.com>
2018-09-15 14:42:49 -0700

    transform uv

--- bin/GLSL/Transform.glsl
+++ bin/GLSL/Transform.glsl
@@ -9,7 +9,7 @@ vec4 Transform()
 {
 	vec2 rs = (vUV+TransformParam.translate) * TransformParam.scale;   
     vec2 ro = vec2(rs.x*cos(TransformParam.rotate) - rs.y * sin(TransformParam.rotate), rs.x*sin(TransformParam.rotate) + rs.y * cos(TransformParam.rotate));
-    vec2 nuv = fract(ro);
+    vec2 nuv = ro;//fract(ro);
 	vec4 tex = texture(Sampler0, nuv);
 	return tex;
 }

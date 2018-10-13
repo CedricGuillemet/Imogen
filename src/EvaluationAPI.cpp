034c84be7eb1009edf53d2aa35c0ca4fb67e0abe
Cedric Guillemet <cedric.guillemet@gmail.com>
2018-10-12 20:25:06 -0700

    cross cubemap shader in comment

--- src/EvaluationAPI.cpp
+++ src/EvaluationAPI.cpp
@@ -1032,9 +1032,35 @@ void Evaluation::NodeUICallBack(const ImDrawList* parent_list, const ImDrawCmd* 
 "vec2 b = sin(ng);"
 			//Color = sampler(Texture, vec3(vec2(s.x, c.x) * c.y, s.y));
 				//"outPixDiffuse = texture(sampler, vec3(vec2(s.x, c.x) * c.y, s.y)); }\n"
-				"outPixDiffuse = texture(sampler, normalize(vec3(a.x*a.y, b.y, b.x*a.y))); }\n"
+				"outPixDiffuse = texture(sampler, normalize(vec3(a.x*a.y, -b.y, b.x*a.y))); }\n"
 //"outPixDiffuse = vec4(1.0,0.0,1.0, 1.0); }\n"
 "#endif\n"
+/*
+draw cross
+vec2 uv = fragCoord / iResolution.xy;
+uv.y = 1.0-uv.y;
+vec4 col = vec4(0.0,0.0,0.0,1.0);
+vec2 ngs = floor(uv * vec2(4.0,3.0)+vec2(1.0,0.0)) * 3.14159 * 0.5;
+vec2 cs = cos(ngs);
+vec2 sn = sin(ngs);
+uv.y = 1.0-uv.y;
+vec3 nd=vec3(0.0,0.0,0.0);
+if (uv.y>0.333 && uv.y<0.666)
+{
+uv.x =  -(fract(uv.x*4.0) * 2.0 - 1.0);
+vec3 d = vec3(uv.x, uv.y*6.0-3.0, 1.0);
+nd = vec3(d.x*cs.x - d.z*sn.x, d.y, d.x*sn.x + d.z*cs.x);
+}
+else
+if (uv.x>0.25 && uv.x<0.5)
+{
+uv.y = fract(uv.y*3.0) * 2.0 - 1.0;
+vec3 d = vec3(uv.x*8.0-3.0, 1.0, uv.y);
+nd = vec3(d.x, d.y*cs.y - d.z*sn.y, d.y*sn.y + d.z*cs.y);
+}
+
+fragColor = texture(iChannel0, nd);
+*/
 			};
 			waitingShader = LoadShader(waitinShaderScript, "WaitingShader");
 		}

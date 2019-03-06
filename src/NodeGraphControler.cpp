1c17570f08acba783f972d2a6c464c224b2373e3
Cedric Guillemet <cedric.guillemet@gmail.com>
2019-03-05 18:06:53 -0800

    default preview back to 1K*1K

--- src/NodeGraphControler.cpp
+++ src/NodeGraphControler.cpp
@@ -32,7 +32,7 @@
 #include "UI.h"
 #include "Utils.h"
 
-NodeGraphControler::NodeGraphControler() : mbMouseDragging(false), mEditingContext(mEvaluationStages, false, 256, 256)
+NodeGraphControler::NodeGraphControler() : mbMouseDragging(false), mEditingContext(mEvaluationStages, false, 1024, 1024)
 {
     mCategoriesCount = 10;
     static const char *categories[] = {

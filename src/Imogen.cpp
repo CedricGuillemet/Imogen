9832f42c092a4f4962b389a37a6436480402a329
Cedric Guillemet <cedric.guillemet@gmail.com>
2019-03-09 15:41:40 -0800

    dev 0.11

--- src/Imogen.cpp
+++ src/Imogen.cpp
@@ -1313,7 +1313,7 @@ void Imogen::ShowTitleBar(Builder *builder)
     ImGui::SameLine();
     // imogen info strings
     ImGui::BeginChildFrame(152, ImVec2(io.DisplaySize.x - butSize.x*4.f - 300, 32.f));
-    ImGui::Text("Imogen 0.10");
+    ImGui::Text("Imogen 0.11");
     if (selectedMaterial != -1)
     {
         Material& material = library.mMaterials[selectedMaterial];

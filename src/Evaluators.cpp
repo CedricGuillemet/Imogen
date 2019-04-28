612a836a3cbe2a58b21a78de5cbf783475dc1f39
Cedric Guillemet <cedric.guillemet@gmail.com>
2019-04-28 11:31:57 -0700

    sdl for hotkey libs

--- src/Evaluators.cpp
+++ src/Evaluators.cpp
@@ -22,6 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
+#include <SDL.h>
 #include <GL/gl3w.h> // Initialize with gl3wInit()
 #include "Evaluators.h"
 #include "EvaluationStages.h"

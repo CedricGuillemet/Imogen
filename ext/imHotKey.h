878778ab2fb50965d82c3f39b3d9f971560a42ae
Cedric Guillemet <cedric.guillemet@microsoft.com>
2019-07-23 15:52:44 -0700

    one more time

--- ext/imHotKey.h
+++ ext/imHotKey.h
@@ -97,14 +97,13 @@ namespace ImHotKey
             int x = 0;
             while (Keys[y][x].lib)
             {
-//#ifdef USE_SDL
+#ifdef USE_SDL
                 if (Keys[y][x].scanCodePage7 == scancode)
-/*#elif WIN32
+#elif WIN32
                 if (Keys[y][x].scanCodePage1 == scancode)
 #else
 #error
 #endif
-*/
                     return Keys[y][x];
                 x++;
             }
@@ -208,7 +207,7 @@ namespace ImHotKey
             if (ImGui::IsKeyPressed(i, false))
             {
                 int imKey;
-#ifdef SDL_h_
+#ifdef USE_SDL
                 imKey = i;
 #elif WIN32
                 imKey = MapVirtualKeyA(i, MAPVK_VK_TO_VSC);
@@ -239,7 +238,7 @@ namespace ImHotKey
                         ImGui::Indent(ofs);
                     }
                 }
-#ifdef SDL_h_
+#ifdef USE_SDL
                 bool& butSwtch = keyDown[key.scanCodePage7];
 #elif WIN32
                 bool& butSwtch = keyDown[key.scanCodePage1];
@@ -316,7 +315,7 @@ namespace ImHotKey
             if (ImGui::IsKeyDown(i))
             {
                 int imKey;
-#ifdef SDL_h_
+#ifdef USE_SDL
                 imKey = i;
 #elif WIN32
                 imKey = MapVirtualKeyA(i, MAPVK_VK_TO_VSC);

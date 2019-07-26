e53b6324cee5c2c8c0bd0f634090654a22314b6e
Cedric Guillemet <cedric.guillemet@gmail.com>
2019-07-25 19:09:16 -0700

    hidden parameters distance node fix

--- bin/Nodes/JS/Distance.js
+++ bin/Nodes/JS/Distance.js
@@ -2,9 +2,9 @@ function Distance(parameters, evaluation, context)
 {
 	const source = evaluation.inputIndices[0];
 	const res = GetEvaluationSize(context, source);
-	if (source != -1 && res == EVAL_OK)
+	if (source != -1 && res[2] == EVAL_OK)
     {
-        parameters.passCount = Math.log2(imgWidth)+3;
+        SetParameter(context, evaluation.targetIndex, "passCount", Math.log2(res[0])+1);
     }    
     return EVAL_OK;
 }
